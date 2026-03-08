5715f21f34f391ecf91f35d3a1d20fd212f4c1ac
Sameer-Dangaura <sameerdangaura1@gmail.com>
2026-03-08 20:26:40 +0545

    docs: add description for lexical scope and scope chaining

--- js_basic/9_scopes.js
+++ js_basic/9_scopes.js
@@ -6,42 +6,39 @@
 // const & let => block scope
 // var => function scope
 
-
-// lexical scope => where the function is declared, not where it's called
+// lexical scope means that a function can access variables from its own scope and from the outer scopes, but not from the inner scopes.
+// Example:
 
 function outer() {
-    let x = 10;
-    function inner() {
-        console.log(x); // 10
-    }
-    inner();
+  let x = 10;
+  function inner() {
+    console.log(x); // 10
+  }
+  inner();
 }
 outer();
 
-
 //* scope chaining
+//: if a variable is not found in the current scope, it looks for it in the outer scope, and so on until it reaches the global scope.
 let a = 100;
 function outer1() {
-    let a = 10;
-    function inner2() {
-        let a = 20;
-        console.log(a); // 20
-    }
-    inner2();
+  let a = 10;
+  function inner2() {
+    let a = 20;
+    console.log(a); // 20
+  }
+  inner2();
 }
 outer1();
 
-
 //Q.
-function one(){
-    if(true){
-        let x = 200;
-        console.log(x); // 10
-        x = 400
-    }
-    console.log(x); // not defined
+function one() {
+  if (true) {
+    let x = 200;
+    console.log(x); // 200
+    x = 400;
+  }
+  console.log(x); // not defined
 }
 
 one();
-
-
